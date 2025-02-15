29d3c1406c8ef3eeab3eee493de295ae10b767c6
Tudor <tudorbadea893@gmail.com>
2025-02-15 17:12:15 +0200

    update

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ from pandas.core.dtypes.common import is_numeric_dtype
 from sklearn.metrics import silhouette_samples, silhouette_score
 
 
-df_indicatori = pd.read_csv("Indicatori.csv", index_col=0)
-df_popLoc = pd.read_csv("PopulatieLocalitati.csv", index_col=0)
+df_indicatori = pd.read_csv("data_in/Indicatori.csv", index_col=0)
+df_popLoc = pd.read_csv("data_in/PopulatieLocalitati.csv", index_col=0)
 indici = list(df_indicatori)[:]
 
 #Cerinta1
 medie_tara = df_indicatori["CFA"].mean()
 cerinta1 = df_indicatori[df_indicatori["CFA"] > medie_tara]
-cerinta1.sort_values(by="CFA", ascending=False).to_csv("Cerinta1.csv")
+cerinta1.sort_values(by="CFA", ascending=False).to_csv("data_out/Cerinta1.csv")
 
 
 #Cerinta2
@@ -25,12 +25,12 @@ def fc(t:pd.Series):
 
 df_merged = df_indicatori.merge(df_popLoc[["Judet","Populatie"]], left_index=True, right_index=True)
 df_grouped = df_merged.groupby(by="Judet").apply(func=fc, include_groups=False)
-df_grouped.to_csv("Cerinta2.csv")
+df_grouped.to_csv("data_out/Cerinta2.csv")
 
 
 #B
 
-df = pd.read_csv("LocationQ.csv", index_col=0)
+df = pd.read_csv("data_in/LocationQ.csv", index_col=0)
 
 def clean_data(t:pd.DataFrame):
     for column in t.columns:
@@ -48,7 +48,7 @@ linkage_matrix = pd.DataFrame(
     data=Z,
     columns=["Cluster1", "Cluster2", "Distance", "Instances"]
 )
-linkage_matrix.to_csv("Linkage_Matrix.csv")
+linkage_matrix.to_csv("data_out/Linkage_Matrix.csv")
 
 dendrogram(Z)
 plt.show()
@@ -57,7 +57,7 @@ clustering = AgglomerativeClustering(n_clusters=3, linkage='ward')
 clustering.fit(df)
 labels = clustering.labels_
 df["Label"] = labels + 1
-df.to_csv("Label_DataFrame.csv")
+df.to_csv("data_out/Label_DataFrame.csv")
 
 sil_score = silhouette_score(df, labels)
 sil_samples = silhouette_samples(df, labels)
